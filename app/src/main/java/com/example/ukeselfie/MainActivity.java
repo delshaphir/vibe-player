22ee2f6a6b63c82cfb225f4af6bd1e1061f56dd7
connormorris <connor3090@bellsouth.net>
2020-02-08 21:50:06 -0600

    Added classifier activity

--- app/src/main/java/com/example/ukeselfie/MainActivity.java
+++ app/src/main/java/com/example/ukeselfie/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ukeselfie;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,16 +19,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Iterator;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 import android.app.ActivityManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button bt;
+    //for CameraIntent
+    private int PICK_IMAGE_REQUEST = 1;
+
+    //setUp for tensorflow
+    public static final int INPUT_SIZE = 224;
+    public static final int IMAGE_MEAN = 128;
+    public static final float IMAGE_STD = 128.0f;
+    public static final String INPUT_NAME = "input";
+    public static final String OUTPUT_NAME = "final_result";
+    public static final String MODEL_FILE = "file:///android_asset/rounded_graph.pb";
+    public static final String LABEL_FILE = "file:///android_asset/retrained_labels.txt";
+
+    public Classifier classifier;
+    public Executor executor = Executors.newSingleThreadExecutor();
+    public TextView TV1;
+    public ImageView iV1;
 
     private int[] rand4(){
         Random rand = new Random();
@@ -84,6 +110,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        TV1 = (TextView) findViewById(R.id.textView1);
+        iV1 = (ImageView) findViewById(R.id.imageView1);
+        initTensorFlowAndLoadModel();
     }
 
     @Override
@@ -123,6 +152,69 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true; // App is in background or foreground
+    }
+
+    private void initTensorFlowAndLoadModel() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    classifier = TensorFlowImageClassifier.create(
+                            getAssets(),
+                            MODEL_FILE,
+                            LABEL_FILE,
+                            INPUT_SIZE,
+                            IMAGE_MEAN,
+                            IMAGE_STD,
+                            INPUT_NAME,
+                            OUTPUT_NAME);
+                } catch (final Exception e) {
+                    throw new RuntimeException("Error initializing TensorFlow!", e);
+                }
+            }
+        });
+    }
+
+    public List<Classifier.Recognition> analyse(Bitmap bitmap)
+    {
+        bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+        final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+        return results;
+    }
+
+    public void selectPhoto(View v)
+    {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            try
+            {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                List<Classifier.Recognition> results = analyse(selectedImage);
+                TV1.setText(results.get(0).toString());
+                Log.d("results", results.get(0).toString().substring(0,3));
+                setPicture(selectedImage);
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //set Picture in the ImageView (iV1)
+    public void setPicture(Bitmap bp)
+    {
+        Bitmap scaledBp =  Bitmap.createScaledBitmap(bp, iV1.getWidth(), iV1.getHeight(), false);
+        iV1.setImageBitmap(scaledBp);
     }
 
 }
